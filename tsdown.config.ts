eede8bb3f7023061231bb704dcd0a7d920ff68e5
AmaseCocoa <cocoa@amase.cc>
2026-07-28 09:02:48 +0900

    chore: removed unused imports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig, type TsdownPlugin } from "tsdown";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
   dts: false,
   clean: true,
-  unbundle: true,
 });
